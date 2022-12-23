f0baf481a3c25ca21333ee020e587f3cd0b67735
Michal Grabczewski <price0321@gmail.com>
2022-12-23 17:08:56 +0100

    update

---
+++ SDPBlazor/Models/ToDoItemModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SDPBlazor.Models
+{
+    public class ToDoItemModel
+    {
+        [StringLength(20, ErrorMessage = "Title is long Max 20 characters")]
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
--- SDPBlazor/Program.cs
+++ SDPBlazor/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
 using SDPBlazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +9,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMudServices(conf =>
+{
+    conf.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
+    conf.SnackbarConfiguration.VisibleStateDuration = 3000;
+});
 
 await builder.Build().RunAsync();
